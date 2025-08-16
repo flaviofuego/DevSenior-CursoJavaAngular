4a89de40fae44827691b19f903e8c36438ba310d
Flavio Arregoces <flavioarregoces@gmail.com>
2025-08-15 21:42:43 -0500

    Agregar configuración inicial del proyecto con Spring Boot, incluyendo archivos de configuración, dependencias y estructura básica.

---
+++ product-demo/src/main/java/com/flaviostack/back/product/GestionDeProductosApplication.java
@@ -0,0 +1,13 @@
+package com.flaviostack.back.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeProductosApplication.class, args);
+	}
+
+}
---
+++ product-demo/src/test/java/com/flaviostack/back/product/GestionDeProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flaviostack.back.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
